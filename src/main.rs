f7e4e8d9fee77970b2d2b3ffa3b8bd6d4ed23360
Pavel Kuzmin <Virus191288@gmail.com>
2023-12-09 15:08:16 +0500

    refactor

--- src/main.rs
+++ src/main.rs
@@ -28,96 +28,51 @@ fn get_token() -> Result<String, Box<dyn std::error::Error>> {
     Ok(token)
 }
 
+fn create_arg(name: &'static str, long: &'static str, short: char, help: &'static str) -> Arg {
+    Arg::new(name)
+        .long(long)
+        .short(short)
+        .help(help)
+        .exclusive(true)
+        .value_parser(value_parser!(String))
+}
+
+fn create_subcommand(name: &'static str, args: &[Arg], about: &'static str) -> Command {
+    let mut command = Command::new(name).about(about);
+    for arg in args {
+        command = command.arg(arg.clone());
+    }
+    command
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = Command::new("gar")
-        .bin_name("gar")
-        .arg(
-            Arg::new("ref")
-                .long("ref")
-                .short('r')
-                .help("The name of the ref tree")
-                .exclusive(true)
-                .value_parser(value_parser!(String)),
-        )
-        .arg(
-            Arg::new("owner")
-                .long("owner")
-                .short('o')
-                .help("The owner of the repository where the action is located.")
-                .exclusive(true)
-                .value_parser(value_parser!(String)),
-        )
-        .arg(
-            Arg::new("repo")
-                .long("repo")
-                .short('p')
-                .help("The name of the repository where the action is located.")
-                .exclusive(true)
-                .value_parser(value_parser!(String)),
-        )
-        .arg(
-            Arg::new("token")
-                .long("token")
-                .short('t')
-                .help("The token used for authentication. If not provided, the GAR_TOKEN environment variable will be used.")
-                .exclusive(true)
-                .value_parser(value_parser!(String)),
-        )
-        .arg(
-            Arg::new("inputs")
-                .long("inputs")
-                .short('i')
-                .help("The name of the event that triggers the action.")
-                .exclusive(true)
-                .value_parser(value_parser!(String)),
-        )
-        .subcommand_required(false)
-        .subcommand(
-            Command::new("history")
-                .about("Shows the history of workflow runs"),
-        ).subcommand(
-            Command::new("details")
-                .about("Shows the details of workflow run"),
-        ).get_matches();
+    let ref_arg = create_arg("ref", "ref", 'r', "The name of the ref tree");
+    let owner_arg = create_arg("owner", "owner", 'o', "The owner of the repository where the action is located.");
+    let repo_arg = create_arg("repo", "repo", 'p', "The name of the repository where the action is located.");
+    let token_arg = create_arg("token", "token", 't', "The token used for authentication. If not provided, the GAR_TOKEN environment variable will be used.");
+    let inputs_arg = create_arg("inputs", "inputs", 'i', "The name of the event that triggers the action.");
 
-    let token = match matches.get_one::<String>("token") {
-        Some(owner_name) => owner_name.to_string(),
-        None => get_token().unwrap(),
-    };
+    let common_args = vec![ref_arg.clone(), owner_arg.clone(), repo_arg.clone(), token_arg.clone()];
 
-    let owner = match matches.get_one::<String>("owner") {
-        Some(owner_name) => owner_name.to_string(),
-        None => get_git_owner()?,
-    };
+    let mut gar_command = Command::new("gar").bin_name("gar");
+    for arg in &common_args {
+        gar_command = gar_command.arg(arg.clone());
+    }
+    gar_command = gar_command.arg(inputs_arg.clone());
 
-    let repo = match matches.get_one::<String>("repo") {
-        Some(repo_name) => repo_name.to_string(),
-        None => get_git_repo()?,
-    };
+    let history_command = create_subcommand("history", &common_args, "Shows the history of workflow runs");
+    let details_command = create_subcommand("details", &common_args, "Shows the details of workflow run");
 
-    let ref_name = match matches.get_one::<String>("ref") {
-        Some(ref_name) => ref_name.to_string(),
-        None => get_git_tree_name().unwrap_or("main".parse().unwrap()),
-    };
+    gar_command = gar_command.subcommand(history_command).subcommand(details_command);
 
-    let inputs = match matches.get_one::<String>("inputs") {
-        Some(inputs) => inputs.to_string(),
-        None => "".to_string(),
-    };
+    let matches = gar_command.subcommand_required(false).get_matches();
 
-    let inputs_collect: HashMap<_, _> = inputs
-        .split('&')
-        .filter_map(|pair| {
-            let mut split = pair.splitn(2, '=');
-            if let (Some(key), Some(value)) = (split.next(), split.next()) {
-                Some((key, value))
-            } else {
-                None
-            }
-        })
-        .collect();
+    let token = matches.get_one::<String>("token").map(|s| s.to_owned()).unwrap_or_else(|| get_token().unwrap());
+    let owner = matches.get_one::<String>("owner").map(|s| s.to_owned()).unwrap_or_else(|| get_git_owner().unwrap());
+    let repo = matches.get_one::<String>("repo").map(|s| s.to_owned()).unwrap_or_else(|| get_git_repo().unwrap());
+    let ref_name = matches.get_one::<String>("ref").map(|s| s.to_owned()).unwrap_or_else(|| get_git_tree_name().unwrap_or("main".parse().unwrap()));
 
     match matches.subcommand() {
         Some(("history", _)) => {
@@ -128,6 +83,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             show_details(&token, &owner, &repo).await?;
         }
         _ => {
+            let inputs = matches.get_one::<String>("inputs").map(|s| s.to_owned()).unwrap_or_else(String::new);
+
+            let inputs_collect: HashMap<_, _> = inputs
+                .split('&')
+                .filter_map(|pair| {
+                    let mut split = pair.splitn(2, '=');
+                    if let (Some(key), Some(value)) = (split.next(), split.next()) {
+                        Some((key, value))
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
             run_workflow(&token, &owner, &repo, &ref_name, inputs_collect).await?;
         },
     };
